6eea8c9998040969f1d01c352a1d610766b8c34f
Alister Scott <alister.scott@gmail.com>
2012-06-04 21:16:46 +1000

    Removed superfluous code that is defined in the base page class

--- lib/pages/wikipedia_base_page.rb
+++ lib/pages/wikipedia_base_page.rb
@@ -1,9 +1,3 @@
 class Wikipedia::BasePage < WikimediaBasePage
 
-  element(:logged_in_link) { |b| b.link(:title => 'Your user page [ctrl-.]') }
-  element(:log_in_link) { |b| b.link(:text => 'Log in / create account') }
-
-  value(:logged_in_as) { |p| p.logged_in_link.text }
-  value(:not_logged_in?) { |p| p.log_in_link.exists? }
-
 end
